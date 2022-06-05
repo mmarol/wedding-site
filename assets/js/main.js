8b7c8cf0152509fa947e0de762b0df92ae138de0
Mrityunjay Marol <6997772+mmarol@users.noreply.github.com>
2022-06-05 10:04:08 -0400

    added form functionality maybe

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
 console.log("test main");
+
+var $form = $("form#wedding-form"),
+	url =
+		"https://script.google.com/macros/s/abcdefghijklmnopqrstuvwxyz1234567890/exec";
+
+$("#submit-form").on("click", function (e) {
+	e.preventDefault();
+	var jqxhr = $.ajax({
+		url: url,
+		method: "GET",
+		dataType: "json",
+		data: $form.serializeObject(),
+	})
+		.success
+		// do something
+		();
+});
