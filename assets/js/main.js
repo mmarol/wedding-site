b7f2b2fd5d2328f9a516f4865aa8fa6280a03cba
Mrityunjay Marol <6997772+mmarol@users.noreply.github.com>
2022-07-26 00:12:06 -0400

    Phase 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,32 @@
-console.log("test main");
+// let attending = $("input:radio[name=attending]");
+// let conditionalField = $(".form__conditional");
 
-var $form = $("form#wedding-form"),
-	url =
-		"https://script.google.com/macros/s/abcdefghijklmnopqrstuvwxyz1234567890/exec";
+// attending.on("change", function () {
+// 	if (this.value == "Yes" || this.value == "Maybe") {
+// 		conditionalField.removeClass("hidden");
+// 	} else {
+// 		conditionalField.addClass("hidden");
+// 	}
+// });
 
-$("#submit-form").on("click", function (e) {
-	e.preventDefault();
-	var jqxhr = $.ajax({
-		url: url,
-		method: "GET",
-		dataType: "json",
-		data: $form.serializeObject(),
-	})
-		.success
-		// do something
-		();
-});
+// window.addEventListener("load", function () {
+// 	const form = document.getElementById("wedding-form");
+// 	form.addEventListener("submit", function (e) {
+// 		e.preventDefault();
+// 		const data = new FormData(form);
+// 		const action = e.target.action;
+// 		fetch(action, {
+// 			method: "POST",
+// 			body: data,
+// 		}).then(() => {
+// 			alert(
+// 				"Thank you for filling out that form! If you have questions or if your plans change, you can email us at "
+// 			);
+// 			$(".form").hide();
+// 			$(".form-success").show();
+// 		});
+// 	});
+// });
+
+// https://script.google.com/macros/s/AKfycbwlde-OicLMZeyrAcZvXZgkPFbYA0yhKy4TlPfVBu-OP4LNn_Fkm1Kx99T5Mogm1Lfc/exec
+// AKfycbwlde-OicLMZeyrAcZvXZgkPFbYA0yhKy4TlPfVBu-OP4LNn_Fkm1Kx99T5Mogm1Lfc
